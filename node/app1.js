3adbf2859566517bbbb20418352e293c57204f73
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-15 01:12:52 +0530

    Day2

--- node/app1.js
+++ node/app1.js
@@ -1,2 +1,11 @@
-console.log("Hello! Vivek");
-//node filename.js - To run the file in terminal
+// console.log("Hello! Vivek");
+// console.log(__filename);
+// console.log(__dirname);
+// console.log(process);
+// //node filename.js - To run the file in terminal
+
+const one = process.argv[2];
+const two = process.argv[3];
+
+let message = `Hi, welcome ${one} ${two}`;
+console.log(message);
